060afd9ac1550ca6089b4be922cc4b4550749c47
Dmitry Kazantsev <mrduk97@gmail.com>
2022-11-09 19:23:08 +0700

    Added some new animations for hover and focus. Added new photos for slider.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,6 +2,8 @@
 
 $(function () {
   initCarousel();
+  initMobileMenu();
+  initModalButtons();
 });
 
 function initCarousel() {
@@ -43,7 +45,9 @@ function initCarousel() {
     $(".advantages__list").slick("resize");
     $(".reviews__wrapper").slick("resize");
   });
+}
 
+function initMobileMenu() {
   const mobileMenuOpenBtn = document.querySelector(".main-nav__toggle");
   mobileMenuOpenBtn.addEventListener("click", () => {
     mobileMenuOpenBtn.classList.toggle("main-nav__toggle--opened");
@@ -52,4 +56,30 @@ function initCarousel() {
     mobileMenuText.classList.toggle("hidden");
     mobileMenu.classList.toggle("shown");
   });
+}
+
+function initModalButtons() {
+  debugger;
+  const openButton = document.querySelector(".js-login");
+  const modal = document.querySelector(".sign-in-modal");
+  const closeButton = document.querySelector(".js-close-modal");
+  const body = document.querySelector(".page__body");
+  openButton.addEventListener("click", showModalAndOverlay);
+  closeButton.addEventListener("click", hideModalAndOverlay);
+  document.addEventListener("click", ({ target }) => {
+    if (modal.classList.contains("shown")) {
+      if (target.classList.contains("show-overlay")) {
+        hideModalAndOverlay();
+      }
+    }
+  });
+  function showModalAndOverlay() {
+    modal.classList.add("shown");
+    body.classList.add("show-overlay");
+  }
+
+  function hideModalAndOverlay() {
+    modal.classList.remove("shown");
+    body.classList.remove("show-overlay");
+  }
 }
